848400d68930d530c487ce2674a153be79223627
Z User <z@container>
2026-08-09 09:00:14 +0000

    Round 14: Add missing Dp import to CoinFlipScreen.kt
    Added: import androidx.compose.ui.unit.Dp
    
    This resolves Unresolved reference: Dp error at lines 679-680

--- app/src/main/java/com/dannyk/toolbox/ui/screens/tools/everyday/CoinFlipScreen.kt
+++ app/src/main/java/com/dannyk/toolbox/ui/screens/tools/everyday/CoinFlipScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
